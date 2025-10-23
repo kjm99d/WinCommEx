f41678b85b47b13985d5f72bead4c66273c39188
Jungmin Kim <kjm99d@naver.com>
2025-10-24 00:26:47 +0900

    fix: DLL 에서 DLL을 링크 할 경우 dllimport 로 정의되는 문제 수정

--- WinCommEx/Export.h
+++ WinCommEx/Export.h
@@ -1,13 +1,10 @@
 #pragma once
 
-#if _DLL	// DLL 빌드 인 경우
-	#ifdef WINCOMMEX_EXPORTS
-		#define WINCOMMEX_API __declspec(dllexport)
-	#else
-		#define WINCOMMEX_API __declspec(dllimport)
-	#endif
-
-#else		// DLL 빌드가 아닌 경우
-
+#ifdef WINCOMMEX_EXPORTS
+#define WINCOMMEX_API __declspec(dllexport)
+#else
+#define WINCOMMEX_API __declspec(dllimport)
 #endif
+
+
 
